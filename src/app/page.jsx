041622077765360b6a68b54b3a8b16ce00e3da08
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-01-24 21:32:57 -0800

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div>
+        Test Home page
+      </div>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.jsx</code>
@@ -85,7 +88,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
