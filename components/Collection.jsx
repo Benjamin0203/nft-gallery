1a39c2428f6562f3207e4e02a2bbe2ed3a52b757
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-01-26 16:16:13 -0800

    --add new Collection component

---
+++ components/Collection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import ButtonGradient from "./shared/ButtonGradient";
+import Section from "./shared/Section";
+
+export default function Collection() {
+  return (
+    <Section id="collection" title="COLLECTION">
+
+      </Section>
+  )
+
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import { GlobalStyles } from '../../components/shared/GlobalStyles';
-import Intro from '../../components/Intro'
-import About from '../../components/About'
+import Intro from '../../components/Intro';
+import About from '../../components/About';
+import Collection from '../../components/Collection';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function Home() {
       <div style={{ marginTop: "5rem" }}>
         <Intro />
         <About />
+        <Collection />
       </div>
     </main>
     </>
