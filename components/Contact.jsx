4ca844dcd56c970d9e2eee173033de6710de0788
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-01-28 15:54:31 -0800

    --add formspree form and endpoint for contact submission

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import Section from "./shared/Section";
+import Box from "./shared/Box";
+import { useForm, ValidationError } from "@formspree/react"
 
 export default function Contact() {
+  const [state, handleSubmit] = useForm("xrgveqov");
+
+  if (state.succeeded) {
+    return <p>Thanks for your submission!</p>;
+  }
   return (
     <Section id="contact" title="CONTACT US">
+   
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
 
+      <textarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+      <ValidationError errors={state.errors} />
+    </form>
     </Section>
   )
 }
